e57f124e2a797b5a96be1682ac7ca83b802e07e7
Air <FreshA1r@outlook.com>
2018-07-01 19:41:55 +0300

    First Deploy

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement):boolean{
     console.log(`Adding article title: ${title.value} and link: ${link.value}`); 
-
+    this.articles.push(new Article(title.value, link.value, 0)); 
+    title.value = "";
+    link.value = "";
     return false;
+  }
+  
+  public articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 1', 'http://angular.io', 3),
+      new Article('Angular 2', 'http://angular.io', 2),
+      new Article('Angular 3', 'http://angular.io', 1)
+    ];
+  }
+
+  sortedArticles(){
+    return this.articles.sort((a: Article, b: Article) => a.votes - b.votes);
   }
 }
